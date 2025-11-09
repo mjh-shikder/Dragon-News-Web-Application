67ad53d6f145e260cc7be268a7f739aa2562bd26
mjh-shikder <mjhsikder@gmail.com>
2025-11-09 11:16:32 +0600

    them customization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-3 ">
-          <img className="w-[350px]" src={logo} alt="" />
-          <p>Journalism Without Fear or Favour</p>
+          <img className="w-[400px]" src={logo} alt="" />
+          <p className="text-accent">Journalism Without Fear or Favour</p>
     </div>
   );
 };
