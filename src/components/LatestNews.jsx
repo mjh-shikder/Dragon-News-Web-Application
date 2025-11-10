c9ff495abebea5c8ec24d615252dd66f65261c28
mjh-shikder <mjhsikder@gmail.com>
2025-11-10 20:30:26 +0600

    Marquee Adjustments

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,7 +5,7 @@ const LatestNews = () => {
     return (
         <div className='flex items-center gap-3 bg-base-200 p-3 rounded-lg '>
             <p className='text-base-100 bg-secondary px-4 py-2 font-semibold rounded-lg text-lg '>Latest</p>
-            <Marquee className='flex  gap-3.5'>
+            <Marquee className='flex  gap-3.5' pauseOnHover={true} speed={100}>
             <p className='font-semibold text-lg text-gray-800'><span className='text-secondary'> Lorem ipsum </span>dolor sit amet, consectetur adipisicing elit. Hic, fuga laboriosam sed eum amet inventore commodi obcaecati provident sunt. Illo assumenda amet  </p>
             </Marquee>
         </div>
