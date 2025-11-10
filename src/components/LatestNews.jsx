3e10437a444c24ecb8c91acccef60060dac7c09c
mjh-shikder <mjhsikder@gmail.com>
2025-11-10 20:25:46 +0600

    Marquee added

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const LatestNews = () => {
+    return (
+        <div className='flex items-center gap-3 bg-base-200 p-3 rounded-lg '>
+            <p className='text-base-100 bg-secondary px-4 py-2 font-semibold rounded-lg text-lg '>Latest</p>
+            <Marquee className='flex  gap-3.5'>
+            <p className='font-semibold text-lg text-gray-800'><span className='text-secondary'> Lorem ipsum </span>dolor sit amet, consectetur adipisicing elit. Hic, fuga laboriosam sed eum amet inventore commodi obcaecati provident sunt. Illo assumenda amet  </p>
+            </Marquee>
+        </div>
+    );
+};
+
+export default LatestNews;
