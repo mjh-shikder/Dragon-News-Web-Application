8677c8faf0d1c1428a256cdfb31f8d07d82950db
mjh-shikder <mjhsikder@gmail.com>
2025-11-12 19:31:20 +0600

    categori section added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { use } from 'react';
 
-const categoryPromise = fetch('/')
+const categoryPromise = fetch('/categories.json').then((res) =>
+    res.json());
 
 const Categories = () => {
+    
+    const categories = use(categoryPromise);
+
+
     return (
         <div>
-           <h2 className='font-bold '>All Categories</h2>
+           <h2 className='font-bold '>All Categories ({categories.length}) </h2>
         </div>
     );
 };
--- src/components/Home/LeftAside.jsx
+++ src/components/Home/LeftAside.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Categories from '../Categories';
 
 const LeftAside = () => {
     return (
         <div>
+            <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
             <Categories></Categories>
+            </Suspense>
         </div>
     );
 };
