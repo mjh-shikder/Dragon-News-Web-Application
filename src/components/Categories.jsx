a18270813a898dfa1998341955e70947404343a3
mjh-shikder <mjhsikder@gmail.com>
2025-11-12 19:54:40 +0600

    hover and active class added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,27 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import { NavLink } from "react-router";
 
-const categoryPromise = fetch('/categories.json').then((res) =>
-    res.json());
+const categoryPromise = fetch("/categories.json").then((res) => res.json());
 
 const Categories = () => {
-    
-    const categories = use(categoryPromise);
+  const categories = use(categoryPromise);
 
-
-    return (
-        <div>
-           <h2 className='font-bold '>All Categories ({categories.length}) </h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="font-bold ">All Categories ({categories.length}) </h2>
+      <div className="grid grid-cols-1 mt-5 gap-2 ">
+        {categories.map((category) => (
+          <NavLink
+            className={" bg-base-100 border-0 font-semibold text-accent hover:underline "}
+            key={category.id}
+            to={`/category/${category.id}`}
+          >
+            {category.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
