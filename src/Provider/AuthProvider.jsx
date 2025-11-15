f3ea4e5abcb526e71c047771356281208f9c4027
mjh-shikder <mjhsikder@gmail.com>
2025-11-15 17:59:26 +0600

    logout function added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,32 +1,40 @@
 import React, { createContext, useEffect, useState } from "react";
 import app from "../Firebase/Firebase.config";
 export const AuthContext = createContext();
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    console.log(user);
-    
+  const [user, setUser] = useState(null);
+  console.log(user);
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-    useEffect(() => {
-      const unsubscribe =  onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
+  const logout = () => {
+    return signOut(auth);
+  };
 
-        })
-        return () => {
-            unsubscribe()
-        }
-    },[])
-    
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const authData = {
     user,
     setUser,
     createUser,
+    logout,
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,51 @@
-import React, { useContext } from 'react';
-import { Link, NavLink } from 'react-router';
-import profileIcon from '../assets/user.png'
-import { AuthContext } from '../Provider/AuthProvider';
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router";
+import profileIcon from "../assets/user.png";
+import { AuthContext } from "../Provider/AuthProvider";
 const Navbar = () => {
-    const {user} = useContext(AuthContext)
-    return (
-        <div className='flex justify-between my-5 items-center'>
-            <div className=''>{ user && user.email }</div>
-            <div className='nav space-x-3 '>
-                <NavLink className={'text-accent font-semibold'} to={'/'}>Home</NavLink>
-                <NavLink className={'text-accent font-semibold'} to={'/about'}>About</NavLink>
-                <NavLink className={'text-accent font-semibold'} to={'/career'}>Career</NavLink>
-            </div>
-            <div className='login-btn flex space-x-3 '>
-                <img src={profileIcon} alt="" />
-                <Link to={'/auth/login'} className='btn btn-primary rounded-lg px-7 '>Login</Link>
-            </div>
-        </div>
-    );
+  const { user, logout } = useContext(AuthContext);
+
+  const handleLogout = (e) => {
+    logout()
+      .then(() => {
+        alert('Loginout Successfully')
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  return (
+    <div className="flex justify-between my-5 items-center">
+      <div className="">{user && user.email}</div>
+      <div className="nav space-x-3 ">
+        <NavLink className={"text-accent font-semibold"} to={"/"}>
+          Home
+        </NavLink>
+        <NavLink className={"text-accent font-semibold"} to={"/about"}>
+          About
+        </NavLink>
+        <NavLink className={"text-accent font-semibold"} to={"/career"}>
+          Career
+        </NavLink>
+      </div>
+      <div className="login-btn flex space-x-3 ">
+        <img src={profileIcon} alt="" />
+        {user ? (
+          <button
+            onClick={handleLogout}
+            className="btn btn-primary rounded-lg px-7 "
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to={"/auth/login"} className="btn btn-primary rounded-lg px-7 ">
+            Login
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
